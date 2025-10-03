2f86dd3c205cdf4ff725b626dd4c8489fcddd72e
Lucia Scalia <luciaxscaliax@gmail.com>
2025-10-02 21:53:58 -0300

    CSS de cronometro y url en context processors

--- Interfaz/context_processors.py
+++ Interfaz/context_processors.py
@@ -1,7 +1,8 @@
 # Interfaz/context_processors.py
-from .models import CalendarioPage
+from .models import CalendarioPage, CronometroPage
 
 def global_pages(request):
     return {
-        "calendario_page": CalendarioPage.objects.live().first()
+        "calendario_page": CalendarioPage.objects.live().first(),
+        "cronometro_page": CronometroPage.objects.live().first(),
     }
